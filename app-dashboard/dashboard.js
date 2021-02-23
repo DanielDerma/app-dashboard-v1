bc4d6b46acb3db02ec19e0b287765e82e2cfbd45
daniel <danielderma94@gmail.com>
2021-02-23 11:10:07 -0700

    tercer commit, todo jala excelente, bs, html, js. Se agregara firebase con auth y luego app funciones

--- app-dashboard/dashboard.js
+++ app-dashboard/dashboard.js
@@ -86,5 +86,59 @@
   window.onload = function () {
     var ctx = document.getElementById("myChart").getContext("2d");
     window.myChart = new Chart(ctx, config);
+    // var ctx2 = document.getElementById("myChart2").getContext("2d");
+    // window.myChart = new Chart(ctx2, config);
+    // var ctx3 = document.getElementById("myChart3").getContext("2d");
+    // window.myChart = new Chart(ctx3, config);
   };
 })();
+
+function dropHandler(ev) {
+  console.log("Fichero(s) arrastrados");
+
+  // Evitar el comportamiendo por defecto (Evitar que el fichero se abra/ejecute)
+  ev.preventDefault();
+
+  if (ev.dataTransfer.items) {
+    // Usar la interfaz DataTransferItemList para acceder a el/los archivos)
+    for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+      // Si los elementos arrastrados no son ficheros, rechazarlos
+      if (ev.dataTransfer.items[i].kind === "file") {
+        var file = ev.dataTransfer.items[i].getAsFile();
+        console.log("... file[" + i + "].name = " + file.name);
+      }
+    }
+  } else {
+    // Usar la interfaz DataTransfer para acceder a el/los archivos
+    for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+      console.log(
+        "... file[" + i + "].name = " + ev.dataTransfer.files[i].name
+      );
+    }
+  }
+
+  // Pasar el evento a removeDragData para limpiar
+  removeDragData(ev);
+}
+
+function dragOverHandler(ev) {
+  console.log("File(s) in drop zone");
+
+  // Prevent default behavior (Prevent file from being opened)
+  ev.preventDefault();
+}
+
+function removeDragData(ev) {
+  console.log("Removing drag data");
+
+  if (ev.dataTransfer.items) {
+    // Use DataTransferItemList interface to remove the drag data
+    ev.dataTransfer.items.clear();
+  } else {
+    // Use DataTransfer interface to remove the drag data
+    ev.dataTransfer.clearData();
+  }
+}
+
+// Doughnut
+// Pie
